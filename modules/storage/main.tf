40c7dccdfa715179ebce317d8b3da0b6f7ac5ba9
Verrel <verrel.angkasa@rsmurniteguh.com>
2026-07-23 16:50:54 +0700

    Update

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -81,17 +81,28 @@ module "ecr" {
   }
 }
 
-# module "s3_bucket" {
-#   source = "terraform-aws-modules/s3-bucket/aws"
+# Provision S3 for frontend static web-hosting
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-#   bucket = "networth-tracker-frontend"
-#   acl    = "private"
+  bucket = "networth-tracker-frontend"
+  acl    = "private"
 
-#   control_object_ownership = true
-#   object_ownership         = "ObjectWriter"
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
 
-#   versioning = {
-#     enabled = true
-#   }
-# }
+  versioning = {
+    enabled = true
+  }
+
+  website = {
+    index_document = ""
+    error_document = ""
+    routing_rules = [{}]
+  }
+
+  tags = {
+    Terraform = "true"
+  }
+}
 
